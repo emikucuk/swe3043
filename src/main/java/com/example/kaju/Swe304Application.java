47d39f1a4edbb951fc8b69bd695bebe14338b90e
Samet Seçer <abdulsamet.secer@ogr.sakarya.edu.tr>
2025-05-08 11:37:17 +0300

    v20 pipeline test

--- src/main/java/com/example/kaju/Swe304Application.java
+++ src/main/java/com/example/kaju/Swe304Application.java
@@ -8,6 +8,6 @@ public class Swe304Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Swe304Application.class, args);
-        System.out.println("CI/CD PR Test v4");
+        System.out.println("CI/CD PR Test v20");
     }
 }
