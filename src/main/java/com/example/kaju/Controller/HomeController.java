01aa95fa0e8b9caa3ff621d3f6a6eac094164424
Emin Küçük <eminkucuk612@gmail.com>
2025-05-22 10:17:49 +0300

    try to reach page

--- src/main/java/com/example/kaju/Controller/HomeController.java
+++ src/main/java/com/example/kaju/Controller/HomeController.java
@@ -1,11 +1,11 @@
 package com.example.kaju.Controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
-    @GetMapping("")
+    @GetMapping("/")
     public String getHome() {
         return "home";
     }
