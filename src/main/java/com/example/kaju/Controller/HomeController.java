acdf99291573189767e5e3af997a7337a71608b9
Emin Küçük <eminkucuk612@gmail.com>
2025-05-22 10:25:48 +0300

    final final test

--- src/main/java/com/example/kaju/Controller/HomeController.java
+++ src/main/java/com/example/kaju/Controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String getHome() {
-        return "home page";
+        return "home page test";
     }
 }
